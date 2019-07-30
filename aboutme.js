4ecf93b4ee4571daf91059042a44e503f9ad9b16
ZiZijourney <53492850+ZiZijourney@users.noreply.github.com>
2019-07-30 15:23:19 -0400

    Add files via upload
    Add JavaScript file and Cascading branch

---
+++ aboutme.js
@@ -0,0 +1,22 @@
+alert("Welcome to my JavaScript Page!");
+
+function makeImageBig(x) {
+    x.style.width = "500px"
+}
+
+function makeImageOriginalSize(x) {
+    x.style.width = "200px"
+}
+
+/* 
+
+function showHideSection(section) {
+    var showHideSection = document.getElementById(section)
+    if (showHideSection.style.display == "none") {
+        showHideSection.style.display = "block";
+        document.getElementById("causes").innerHTML = "click me!";
+    } else {
+        showHideSection.style.display = "none";
+        document.getElementById("causes").innerHTML = "click me!";
+    }
+} */
